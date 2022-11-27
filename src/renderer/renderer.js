a0406b3798c7d19d2de6e003369c29b7bf65d6dc
wov <wovfeng@gmail.com>
2022-11-27 23:36:14 +0800

    request animtion frame

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,24 +1,38 @@
 // renderer.js
 
 let x = 0;
+let s;
+let i;
+
+let lastTimestamp = 0,
+    maxFPS = 10,
+    timestep = 1000 / maxFPS; // ms for each frame
+
 
 export function render(stage,Idle){
-    console.log(Idle);
-    const ctx = stage.getContext("2d");
+    s = stage;
+    i = Idle;
+    window.requestAnimationFrame(step);
+}
+
+function step(timestamp){ 
+    window.requestAnimationFrame(step);
+
+    // keep 60fps.
+    // console.log(timestamp)
+    if (timestamp - lastTimestamp < timestep) return;
+    lastTimestamp = timestamp;
+    
+    if(!s) return;
+
+    const ctx = s.getContext("2d");
     // ctx.drawImage(Idle, 0, 0);
     const sx = 128 * x;
     // clear canvas..
-    stage.width = stage.width;
+    s.width = s.width;
     // draw image..
-    ctx.drawImage(Idle, sx, 0, 128, 128, 0, 0, 128, 128);
+    ctx.drawImage(i, sx, 0, 128, 128, 0, 0, 128, 128);
     x++;
     if(x >= 8){x = 0;}
-    
-    //TODO: change to requsetanimation Frame..
-
-    // window.requestAnimationFrame(render(stage,Idle));
-
-    setInterval( ()=>{render(stage,Idle)},16 );
-
 
 }
